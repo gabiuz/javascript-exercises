42a2d5a5b000160228d2e7789e9c2254ab34070a
gabiuz <pelagionicolai@gmail.com>
2026-01-05 21:38:16 +0800

    done 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num, ...otherNum) {
+  return arr.filter((number) => number !== num && !otherNum.includes(number));
 };
 
 // Do not edit below this line
