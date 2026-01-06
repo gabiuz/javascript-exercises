79f3de1cdc8f4c5ac260b6c4348c1347c69bb535
gabiuz <pelagionicolai@gmail.com>
2026-01-06 17:39:58 +0800

    done 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  if (firstNum < 0 || !Number.isInteger(firstNum)) {
+    return "ERROR";
+  } else if (secondNum < 0 || !Number.isInteger(secondNum)) {
+    return "ERROR";
+  } else {
+    let largerNum;
+    let smallerNum;
+    if (firstNum > secondNum) {
+      largerNum = firstNum;
+      smallerNum = secondNum;
+    } else {
+      largerNum = secondNum;
+      smallerNum = firstNum;
+    }
+    let numInBetween = [];
+    while (smallerNum <= largerNum) {
+      numInBetween.push(smallerNum);
+      smallerNum++;
+    }
+    const total = numInBetween.reduce((total, num) => total + num, 0);
+    return total;
+  }
 };
 
 // Do not edit below this line
